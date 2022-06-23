de7b618fc1ec667055baff9441e919cd1770c754
Busybeebee11 <105250713+Busybeebee11@users.noreply.github.com>
2022-06-23 21:17:07 +0100

    added dot to nigeria

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var myName = "Blessing Olusiyan. \n"
 var height = "5ft2in tall \n"
-var country = "Nigeria \n"
+var country = "Nigeria. \n"
 
 alert("My name is " + myName + "I'm " + height + "I'm from " + country)
